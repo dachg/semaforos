6b59a2125a332cd51897fbc90cd2eda70b2ea70a
Diego Chacon Gomez <dchacon@adgasd32.int>
2021-11-09 21:28:26 -0500

    Semaforos

---
+++ Cliente/Cliente/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Cliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.g1_red = new System.Windows.Forms.Panel();
+            this.g1_yellow = new System.Windows.Forms.Panel();
+            this.g1_green = new System.Windows.Forms.Panel();
+            this.g2_red = new System.Windows.Forms.Panel();
+            this.g2_yellow = new System.Windows.Forms.Panel();
+            this.g2_green = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.g1_amount = new System.Windows.Forms.TextBox();
+            this.g1_ligth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.g1_report = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.g2_amount = new System.Windows.Forms.TextBox();
+            this.g2_ligth = new System.Windows.Forms.TextBox();
+            this.g2_report = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // g1_red
+            // 
+            this.g1_red.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.g1_red.Location = new System.Drawing.Point(204, 82);
+            this.g1_red.Name = "g1_red";
+            this.g1_red.Size = new System.Drawing.Size(50, 50);
+            this.g1_red.TabIndex = 0;
+            // 
+            // g1_yellow
+            // 
+            this.g1_yellow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.g1_yellow.Location = new System.Drawing.Point(204, 138);
+            this.g1_yellow.Name = "g1_yellow";
+            this.g1_yellow.Size = new System.Drawing.Size(50, 50);
+            this.g1_yellow.TabIndex = 1;
+            // 
+            // g1_green
+            // 
+            this.g1_green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.g1_green.Location = new System.Drawing.Point(204, 194);
+            this.g1_green.Name = "g1_green";
+            this.g1_green.Size = new System.Drawing.Size(50, 50);
+            this.g1_green.TabIndex = 1;
+            // 
+            // g2_red
+            // 
+            this.g2_red.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.g2_red.Location = new System.Drawing.Point(539, 82);
+            this.g2_red.Name = "g2_red";
+            this.g2_red.Size = new System.Drawing.Size(50, 50);
+            this.g2_red.TabIndex = 0;
+            // 
+            // g2_yellow
+            // 
+            this.g2_yellow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.g2_yellow.Location = new System.Drawing.Point(539, 138);
+            this.g2_yellow.Name = "g2_yellow";
+            this.g2_yellow.Size = new System.Drawing.Size(50, 50);
+            this.g2_yellow.TabIndex = 1;
+            // 
+            // g2_green
+            // 
+            this.g2_green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.g2_green.Location = new System.Drawing.Point(539, 194);
+            this.g2_green.Name = "g2_green";
+            this.g2_green.Size = new System.Drawing.Size(50, 50);
+            this.g2_green.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grupo 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Grupo 2";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // g1_amount
+            // 
+            this.g1_amount.Location = new System.Drawing.Point(194, 281);
+            this.g1_amount.Name = "g1_amount";
+            this.g1_amount.Size = new System.Drawing.Size(100, 20);
+            this.g1_amount.TabIndex = 3;
+            // 
+            // g1_ligth
+            // 
+            this.g1_ligth.Location = new System.Drawing.Point(194, 307);
+            this.g1_ligth.Name = "g1_ligth";
+            this.g1_ligth.Size = new System.Drawing.Size(100, 20);
+            this.g1_ligth.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad de semáforos";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Luces fallando";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // g1_report
+            // 
+            this.g1_report.Location = new System.Drawing.Point(204, 333);
+            this.g1_report.Name = "g1_report";
+            this.g1_report.Size = new System.Drawing.Size(90, 23);
+            this.g1_report.TabIndex = 4;
+            this.g1_report.Text = "Enviar reporte";
+            this.g1_report.UseVisualStyleBackColor = true;
+            this.g1_report.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(397, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Cantidad de semáforos";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(436, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Luces fallando";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // g2_amount
+            // 
+            this.g2_amount.Location = new System.Drawing.Point(529, 281);
+            this.g2_amount.Name = "g2_amount";
+            this.g2_amount.Size = new System.Drawing.Size(100, 20);
+            this.g2_amount.TabIndex = 3;
+            // 
+            // g2_ligth
+            // 
+            this.g2_ligth.Location = new System.Drawing.Point(529, 307);
+            this.g2_ligth.Name = "g2_ligth";
+            this.g2_ligth.Size = new System.Drawing.Size(100, 20);
+            this.g2_ligth.TabIndex = 3;
+            // 
+            // g2_report
+            // 
+            this.g2_report.Location = new System.Drawing.Point(539, 333);
+            this.g2_report.Name = "g2_report";
+            this.g2_report.Size = new System.Drawing.Size(90, 23);
+            this.g2_report.TabIndex = 4;
+            this.g2_report.Text = "Enviar reporte";
+            this.g2_report.UseVisualStyleBackColor = true;
+            this.g2_report.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.g2_report);
+            this.Controls.Add(this.g2_ligth);
+            this.Controls.Add(this.g1_report);
+            this.Controls.Add(this.g2_amount);
+            this.Controls.Add(this.g1_ligth);
+            this.Controls.Add(this.g1_amount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.g2_green);
+            this.Controls.Add(this.g1_green);
+            this.Controls.Add(this.g2_yellow);
+            this.Controls.Add(this.g2_red);
+            this.Controls.Add(this.g1_yellow);
+            this.Controls.Add(this.g1_red);
+            this.Name = "Form1";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel g1_red;
+        private System.Windows.Forms.Panel g1_yellow;
+        private System.Windows.Forms.Panel g1_green;
+        private System.Windows.Forms.Panel g2_red;
+        private System.Windows.Forms.Panel g2_yellow;
+        private System.Windows.Forms.Panel g2_green;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox g1_amount;
+        private System.Windows.Forms.TextBox g1_ligth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button g1_report;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox g2_amount;
+        private System.Windows.Forms.TextBox g2_ligth;
+        private System.Windows.Forms.Button g2_report;
+    }
+}
+
